600210b91453372b71bba81792320c9be8aba8df
chol413k <holiveros@transbank.cl>
2021-05-24 23:24:32 -0400

    fix: Creacion por defecto de directorios de descargas

--- servel_scraper/main.py
+++ servel_scraper/main.py
@@ -9,8 +9,10 @@ DEFAULT_PDF_DOWNLOAD_PATH = os.path.join(current_path, '..', 'out', 'pdf')
 DEFAULT_GENERATED_CSV_PATH = os.path.join(current_path, '..', 'out', 'csv')
 
 if __name__ == '__main__':
+    os.makedirs(DEFAULT_PDF_DOWNLOAD_PATH)
+    os.makedirs(DEFAULT_GENERATED_CSV_PATH)
     p = ServelPipeline(DEFAULT_PDF_DOWNLOAD_PATH, DEFAULT_GENERATED_CSV_PATH)
     p.run_pipeline([
-        # PipelineStage.DOWNLOAD_PDFS,
+        PipelineStage.DOWNLOAD_PDFS,
         PipelineStage.EXTRACT_CSV_FROM_PDF
     ])
